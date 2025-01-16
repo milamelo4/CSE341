08f5ef9137e1f9feaa2e37d72c286dd37e15616c
Camila Melo <milamelofl@yahoo.com>
2025-01-16 14:24:51 -0700

    removed unnecessary lines

--- server.js
+++ server.js
@@ -5,9 +5,7 @@
 
 const express = require('express');
 const app = express();
-const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
-const router = require('./routes/');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
