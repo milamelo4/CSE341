c3e40e5c5023a815a66f0a5e92a1a1124a12db2a
Camila Melo <milamelofl@yahoo.com>
2025-01-30 13:25:13 -0700

    testing

--- swagger.js
+++ swagger.js
@@ -9,10 +9,12 @@ const doc = {
     process.env.NODE_ENV === "production"
       ? "cse341-4zv7.onrender.com"
       : "localhost:3000", // Dynamic host based on environment
-  schemes: ["http"],
+  schemes: [process.env.NODE_ENV === "production" ? "https" : "http"], // Dynamic schemes based on environment
+  paths: {}, // Add your paths here
 };
 
 const outputFile = "./swagger.json";
 const endpointsFiles = ["./routes/index.js"];
 
-swaggerAutogen(outputFile, endpointsFiles);
+// Pass doc to swaggerAutogen to generate the swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
